d01ada059969299b2a3b68c3e853a02fc0c39962
VershimaKelvin <kelvinityavyar@gmail.com>
2022-01-08 13:54:00 +0100

    i added firebase to the app

--- lib/UI/Home.dart
+++ lib/UI/Home.dart
@@ -9,30 +9,39 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Provider')),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Settings()));
-              },
-              child: Icon(
-                Icons.settings,
-              ),
+    return ChangeNotifierProvider(
+      create: (BuildContext context)=>SettingsProvider(),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Center(child: Text('Provider')),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Settings()));
+                    },
+                    child: Icon(
+                      Icons.settings,
+                    ),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
-      body: Center(
-        child: Text(
-            Provider.of<SettingsProvider>(context).getUnits(),
-          style: TextStyle(
-            color: Colors.grey,
-          ),
-        ),
+            body: Center(
+                  child: Text(
+                      Provider.of<SettingsProvider>(context).getUnits(),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+
+
+          );
+        }
       ),
     );
   }
